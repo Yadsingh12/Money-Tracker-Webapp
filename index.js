62806494180d3d176b099e3ec8f38872099c98e0
Yadsingh12 <your_donotreply_email_id>
2024-09-07 18:20:11 +0530

    feat: setup basic Express server with a root route

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("successfully connected");
+}).listen(5000);
+
+console.log("listening on port 5000")
